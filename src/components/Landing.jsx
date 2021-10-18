0a460114a45ab0605c55f4e6ff06960e743d5202
Michael Ignat <michael.ignat13@gmail.com>
2021-10-18 16:41:58 +0800

    Fixed landing button link

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import UndrawBooks from "../assets/Undraw_Books.svg";
 
 const Landing = () => {
@@ -12,9 +11,9 @@ const Landing = () => {
             <h2>
               Find your dream book with <span className="purple">Library</span>
             </h2>
-            <Link to="#features">
+            <a href="#features">
               <button className="btn">Browse books</button>
-            </Link>
+            </a>
           </div>
           <figure className="header__img--wrapper">
             <img src={UndrawBooks} alt="" />
